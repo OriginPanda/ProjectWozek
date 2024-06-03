0f3a623153d3102a938a3229ad471fc4fdc25154
Jedrzejpiernik <151682979+Jedrzejpiernik@users.noreply.github.com>
2024-06-04 00:17:55 +0200

    Update Midas.py

--- Midas.py
+++ Midas.py
@@ -73,16 +73,18 @@ def capture_live_video(camera_index=0, center_mean_threshold=default_th):
 
             # Sprawdzenie, czy średnia wartość w centralnym regionie jest mniejsza niż określony próg
             if center_mean < center_mean_threshold:
-                print("Przeszkoda w centralnym regionie.")
-                if left_mean > right_mean:
+                print("Średnia wartość w regionie centralnym jest mniejsza niż próg")
+                direction = "do przodu"
+            else:
+                if max_region == "lewo":
+                    direction = "w prawo"
+                elif max_region == "prawo":
                     direction = "w lewo"
                 else:
-                    direction = "w prawo"
-            else:
-                direction = "do przodu"
+                    direction = "do przodu"
             
             # Aktualizacja etykiety kierunku w GUI
-            direction_label.config(text=f"Kierunek: Skręć {direction.capitalize()}")
+            direction_label.config(text=f"Kierunek: Skręć {min_region.capitalize()}")
             
             # Wizualizacja mapy głębokości
             color_map = plt.get_cmap('jet_r')
@@ -93,7 +95,7 @@ def capture_live_video(camera_index=0, center_mean_threshold=default_th):
             font_scale = 1.2
             thickness = 3
             color = (255, 255, 255)  # biały kolor
-            text = f"Skręć {direction.capitalize()}"
+            text = f"Skręć {min_region.capitalize()}"
             text_size = cv2.getTextSize(text, font, font_scale, thickness)[0]
             text_x = (colorful_output.shape[1] - text_size[0]) // 2
             text_y = text_size[1] + 10
